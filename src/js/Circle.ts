69a5fe2b4910f94743ec85f1be7b0c5d6021c107
Eric Zanchi <emzanchi@gmail.com>
2017-05-15 10:46:59 -0500

    Added mass

--- src/js/Circle.ts
+++ src/js/Circle.ts
@@ -3,13 +3,15 @@ import Delta from './Delta';
 export default class Circle{
   acceleration: Delta;
   color: string;
+  mass: number;
   radius: number;
   velocity: Delta;
 
-  constructor(radius: number){
+  constructor(radius: number, mass?: number){
     this.acceleration = new Delta;
     this.color = '#' + Math.round(((radius - 15) /10) * 255).toString(16) + '0000';
     this.radius = radius;
+    this.mass = mass || 4/3 * Math.PI * radius ** 3; // Volume of a sphere
     this.velocity = new Delta;
   }
 }
