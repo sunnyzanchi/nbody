c248951bb9c85e23ad269d94e00839dad15e5c60
Eric Zanchi <emzanchi@gmail.com>
2017-05-22 13:33:55 -0500

    Updated constructor arguments

--- src/js/Circle.ts
+++ src/js/Circle.ts
@@ -16,19 +16,19 @@ export default class Circle implements Point{
     mass,
     radius,
     velocity = new Delta,
-    x = 0,
-    y = 0
+    x,
+    y
   }: {
     acceleration?: Delta,
     color?: string,
     mass?: number,
-    radius?: number,
+    radius: number,
     velocity?: Delta,
-    x?: number,
-    y?: number
+    x: number,
+    y: number
   }){
     this.acceleration = acceleration;
-    this.radius = radius || Math.random() * 20 + 5;
+    this.radius = radius;
     this.color = color || '#' + Math.round(((this.radius - 15) / 10) * 255).toString(16) + '0000';
     // Area of a circle
     this.mass = mass || Math.PI * this.radius ** 2;
