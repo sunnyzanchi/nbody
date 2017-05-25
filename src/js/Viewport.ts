f7924ff002116be51ce021e13d82df4a168bcba6
Eric Zanchi <emzanchi@gmail.com>
2017-05-25 12:53:52 -0500

    Added viewport class

---
+++ src/js/Viewport.ts
@@ -0,0 +1,48 @@
+import Point from './Point';
+
+class Viewport implements Point{
+  holding: boolean;
+  lastX: number;
+  lastY: number;
+  max: number;
+  min: number;
+  scale: number;
+  scaleMax: number;
+  scaleMin: number;
+  x: number;
+  y: number;
+
+  constructor({max, min}){
+    this.holding = false;
+    this.lastX = 0;
+    this.lastY = 0;
+    this.max = 10000 || max;
+    this.min = -10000 || min;
+    this.scale = 1;
+    this.scaleMax = 2;
+    this.scaleMin = .01;
+    this.x = 0;
+    this.y = 0;
+  }
+  startHolding({x, y}: Point){
+    this.holding = true;
+    this.lastX = x;
+    this.lastY = y
+  }
+  move({x, y}: Point){
+    if(!this.holding) return;
+    this.x += x - this.lastX;
+    this.y += y - this.lastY;
+
+    this.lastX = x;
+    this.lastY = y
+  }
+  stopHolding(){
+    this.holding = false;
+  }
+  zoom(amount: number){
+    if((amount > 0 && this.scale + amount < this.scaleMax) ||
+       (amount < 0 && this.scale + amount > this.scaleMin))
+      this.scale += amount;
+  }
+}
