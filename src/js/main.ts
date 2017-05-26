db689c2fb80a9eea8462713e9a034d29bf5598e6
Eric Zanchi <emzanchi@gmail.com>
2017-05-26 12:58:36 -0500

    Updated to show an orbit

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,12 +1,12 @@
 import Body from 'Classes/Body';
+import Delta from 'Classes/Delta';
 import Planet from 'Classes/Planet';
 import Star from 'Classes/Star';
 import Universe from 'Classes/Universe';
 
 const universe = new Universe(.5, 8);
-universe.bodies.push(new Star({x: 400, y: 300, radius: 20, mass: 3000}));
-universe.bodies.push(new Planet({x: 200, y: 200, radius: 10, color: '#f99'}));
-universe.bodies.push(new Planet({x: 300, y: 200, radius: 10, color: '#f99'}));
+universe.bodies.push(new Star({x: 400, y: 400, radius: 80, mass: 300000}));
+universe.bodies.push(new Planet({x: 50, y: 400, radius: 10, color: '#f99', velocity: new Delta(0, -20)}));
 
 const canvas: HTMLCanvasElement = document.querySelector('canvas');
 const {width, height} = canvas;
