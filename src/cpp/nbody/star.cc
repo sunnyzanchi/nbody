a3f0852d7fcec2688a70f91cd321bce13bc36f71
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 23:26:24 -0400

    Add Star implementation

---
+++ src/cpp/nbody/star.cc
@@ -0,0 +1,23 @@
+#include "star.h"
+
+namespace nbody {
+
+// TODO: Implement Draw().
+void Star::Draw(/* CanvasRenderingContext2D ctx */) {
+/*
+const {x, y, radius: r} = this;
+
+ctx.beginPath();
+ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+
+const gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
+gradient.addColorStop(0, '#FFFDE7');
+gradient.addColorStop(.7, '#FFF9C4');
+gradient.addColorStop(1, 'transparent');
+
+ctx.fillStyle = gradient;
+ctx.fill();
+*/
+}
+
+} // namespace nbody
