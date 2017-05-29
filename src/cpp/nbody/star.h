ca7af825c0f5da3507d4af351806eba267683b2b
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 22:18:12 -0400

    Format the code

--- src/cpp/nbody/star.h
+++ src/cpp/nbody/star.h
@@ -6,32 +6,32 @@
 
 namespace nbody {
 
-  class Star : public Body, public Drawable {
-  public:
-    Star(float x, float y, float mass, float radius, std::string color,
-          Vector velocity = Vector(), Vector acceleration = Vector())
-      : Body(x, y, mass, radius, color, velocity, acceleration)
-    {}
+class Star : public Body, public Drawable {
+public:
+  Star(float x, float y, float mass, float radius, std::string color,
+        Vector velocity = Vector(), Vector acceleration = Vector())
+    : Body(x, y, mass, radius, color, velocity, acceleration)
+  {}
 
-    // TODO: Implement Draw().
-    void Draw(/* CanvasRenderingContext2D ctx */) {
-      /*
-      const {x, y, radius: r} = this;
+  // TODO: Implement Draw().
+  void Draw(/* CanvasRenderingContext2D ctx */) {
+    /*
+    const {x, y, radius: r} = this;
 
-      ctx.beginPath();
-      ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+    ctx.beginPath();
+    ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
 
-      const gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
-      gradient.addColorStop(0, '#FFFDE7');
-      gradient.addColorStop(.7, '#FFF9C4');
-      gradient.addColorStop(1, 'transparent');
+    const gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
+    gradient.addColorStop(0, '#FFFDE7');
+    gradient.addColorStop(.7, '#FFF9C4');
+    gradient.addColorStop(1, 'transparent');
 
-      ctx.fillStyle = gradient;
-      ctx.fill();
-      */
-    }
-    ~Star() {}
-  };
+    ctx.fillStyle = gradient;
+    ctx.fill();
+    */
+  }
+  ~Star() {}
+};
 
 } // namespace nbody
 
