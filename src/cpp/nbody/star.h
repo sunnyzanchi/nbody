62c8d2e796243e071778f6eb92878c8c900a0d81
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 23:55:21 -0400

    Remove duplicate implementation of Draw()

--- src/cpp/nbody/star.h
+++ src/cpp/nbody/star.h
@@ -13,24 +13,10 @@ public:
     : Body(x, y, mass, radius, color, velocity, acceleration)
   {}
 
-  // TODO: Implement Draw().
-  void Draw(/* CanvasRenderingContext2D ctx */) {
-    /*
-    const {x, y, radius: r} = this;
-
-    ctx.beginPath();
-    ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
-
-    const gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
-    gradient.addColorStop(0, '#FFFDE7');
-    gradient.addColorStop(.7, '#FFF9C4');
-    gradient.addColorStop(1, 'transparent');
-
-    ctx.fillStyle = gradient;
-    ctx.fill();
-    */
-  }
   ~Star() {}
+
+  // TODO: Implement Draw().
+  void Draw(/* CanvasRenderingContext2D ctx */);
 };
 
 } // namespace nbody
