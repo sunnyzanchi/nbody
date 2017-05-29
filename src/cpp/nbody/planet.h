a97a2f34fa5c53dc1aefeb2dee83b29e11b2d1da
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-29 00:28:49 -0400

    Fix double interface implementation

--- src/cpp/nbody/planet.h
+++ src/cpp/nbody/planet.h
@@ -2,11 +2,10 @@
 #define NBODY_PLANET_H_
 
 #include "body.h"
-#include "drawable.h"
 
 namespace nbody {
 
-class Planet : public Body, public Drawable {
+class Planet : public Body {
  public:
   explicit Planet(float x, float y, float mass, float radius, std::string color,
         Vector velocity = Vector(), Vector acceleration = Vector())
