e8b9e2397d21d5e27b21987d4e114ce667a4ceb1
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 23:43:46 -0400

    Reorder functions in header and .cc

--- src/cpp/nbody/vector.cc
+++ src/cpp/nbody/vector.cc
@@ -17,13 +17,14 @@ Vector Vector::Abs() const {
   return Vector(fabs(x_), fabs(y_));
 }
 
-Vector Vector::Add(const Vector& vector) const {
-  // possible overflow
-  return Vector(x_ + vector.x_, y_ + vector.y_);
-}
 Vector Vector::Add(float addend) const {
   // possible overflow
   return Vector(x_ + addend, y_ + addend);
+}
+
+Vector Vector::Add(const Vector& addend) const {
+  // possible overflow
+  return Vector(x_ + vector.x_, y_ + vector.y_);
 }
 
 Vector Vector::Clone() const {
@@ -35,6 +36,10 @@ float Vector::Cross(const Vector& vector) const {
   return (x_ * vector.y_ - y_ * vector.x_);
 }
 
+float Vector::Distance(const Vector& vector) const {
+  return sqrt(DistanceSquared(vector));
+}
+
 float Vector::DistanceSquared(const Vector& vector) const {
   // possible underflow and overflow
   float dx = x_ - vector.x_;
@@ -42,14 +47,11 @@ float Vector::DistanceSquared(const Vector& vector) const {
   return dx * dx + dy * dy;
 }
 
-float Vector::Distance(const Vector& vector) const {
-  return sqrt(DistanceSquared(vector));
-}
-
 Vector Vector::Divide(float divisor) const {
   // possible div by zero
   return Vector(x_ / divisor, y_ / divisor);
 }
+
 Vector Vector::Divide(const Vector& divisor) const {
   // possible div by zero
   return Vector(x_ / divisor.x_, y_ / divisor.y_);
@@ -72,7 +74,6 @@ float Vector::LengthSquared() const {
   return Dot(this);
 }
 
-// "BilinearlyInterpolate" or "LinearlyInterpolate"
 Vector Vector::Lerp(const Vector& vector, float amount) const {
   // possible underflow and overflow
   // TODO: check that this is correct interpolation (by the definition).
@@ -83,6 +84,7 @@ Vector Vector::Multiply(Vector& multiplier) const {
   // possible overflow
   return Vector(x_ * multiplier.x_, y_ * multiplier.y_);
 }
+
 Vector Vector::Multiply(float multiplier) const {
   // possible overflow
   return Vector(x_ * multiplier, y_ * multiplier);
@@ -92,17 +94,14 @@ Vector Vector::Normalize() const {
   return Divide(Length());
 }
 
-// Changed this to not return anything. Set does not return
-// if we are to be efficient.
-void Vector::Set(Vector target) {
-  x_ = target.x_;
-  y_ = target.y_;
-}
-// Changed this to not return anything. Set should probably not return 
-// an object.
 void Vector::Set(float target) {
   x_ = target;
   y_ = target;
+}
+
+void Vector::Set(Vector target) {
+  x_ = target.x_;
+  y_ = target.y_;
 }
 
 // DANGER: this function (which was likely written incorrectly from the 
@@ -114,13 +113,14 @@ void Vector::SetLength(float length) {
   y_ = temp.y_;
 }
 
-Vector Vector::Subtract(Vector sub) const {
-  // possible underflow
-  return Vector(x_ - sub.x_, y_ - sub.y_);
-}
 Vector Vector::Subtract(float sub) const {
   // possible underflow
   return Vector(x_ - sub, y_ - sub);
+}
+
+Vector Vector::Subtract(Vector sub) const {
+  // possible underflow
+  return Vector(x_ - sub.x_, y_ - sub.y_);
 }
 
 void Vector::Truncate(float max_length) {
--- src/cpp/nbody/vector.h
+++ src/cpp/nbody/vector.h
@@ -9,66 +9,73 @@ namespace nbody {
 // the purposes of a high-performance n-body simulation. This is only 
 // a provisional starting point.
 class Vector {
-protected:
-  float x_, y_;
-  float Dot(const Vector* vector) const;
-public:
+ public:
   explicit Vector(float x = 0, float y = 0);
 
   ~Vector() {}
 
   Vector Abs() const;
 
-  Vector Add(const Vector& vector) const;
   Vector Add(float addend) const;
+
+  Vector Add(const Vector& addend) const;
 
   Vector Clone() const;
 
-  float Cross(const Vector& vector) const;
+  float  Cross(const Vector& vector) const;
 
-  float DistanceSquared(const Vector& vector) const;
+  float  Distance(const Vector& vector) const;
 
-  float Distance(const Vector& vector) const;
+  float  DistanceSquared(const Vector& vector) const;
 
   Vector Divide(float divisor) const;
+
   Vector Divide(const Vector& divisor) const;
 
-  float Dot(const Vector& vector) const;
+  float  Dot(const Vector& vector) const;
 
-  bool Equals(const Vector& vector) const;
+  bool   Equals(const Vector& vector) const;
 
-  float Length() const;
+  float  Length() const;
 
-  float LengthSquared() const;
+  float  LengthSquared() const;
 
   // "BilinearlyInterpolate" or "LinearlyInterpolate"
   Vector Lerp(const Vector& vector, float amount) const;
 
   Vector Multiply(Vector& multiplier) const;
+
   Vector Multiply(float multiplier) const;
 
   Vector Normalize() const;
 
   // Changed this to not return anything. Set does not return
   // if we are to be efficient.
-  void Set(Vector target);
+  void   Set(Vector target);
   // Changed this to not return anything. Set should probably not return 
   // an object.
-  void Set(float target);
+  void   Set(float target);
 
   // DANGER: this function (which was likely written incorrectly from the 
   // original Vector.ts) did not originally mutate to the new length. 
   // Instead, it returned Normalize().Multiply().
-  void SetLength(float length);
+  void   SetLength(float length);
 
   Vector Subtract(Vector sub) const;
+
   Vector Subtract(float sub) const;
 
-  void Truncate(float max_length);
+  void   Truncate(float max_length);
 
   Vector GetReverse() {
-    return Multiply(-1);
+    return Multiply(-1.0);
   }
+ protected:
+  float x_;
+
+  float y_;
+
+  float Dot(const Vector* vector) const;
 };
 
 } // namespace nbody
