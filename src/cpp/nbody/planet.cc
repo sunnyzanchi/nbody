2c555436f90be672853c867235186af195b0a592
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 23:16:37 -0400

    Outsource implementation to .cc

---
+++ src/cpp/nbody/planet.cc
@@ -0,0 +1,15 @@
+#include "planet.h"
+
+namespace nbody {
+
+// TODO: Implement Draw().
+void Planet::Draw(/* CanvasRenderingContext2D ctx */) {
+ /*
+ ctx.beginPath();
+ ctx.ellipse(x_, y_, radius_, radius_, 0, 0, 2 * 3.1415);
+ ctx.fillStyle = color_;
+ ctx.fill();
+ */
+}
+
+} // namespace nbody
--- src/cpp/nbody/planet.h
+++ src/cpp/nbody/planet.h
@@ -7,22 +7,14 @@
 namespace nbody {
 
 class Planet : public Body, public Drawable {
-public:
+ public:
   explicit Planet(float x, float y, float mass, float radius, std::string color,
         Vector velocity = Vector(), Vector acceleration = Vector())
     : Body(x, y, mass, radius, color, velocity, acceleration)
   {}
-
-  // TODO: Implement Draw().
-  void Draw(/* CanvasRenderingContext2D ctx */) {
-    /*
-    ctx.beginPath();
-    ctx.ellipse(x_, y_, radius_, radius_, 0, 0, 2 * 3.1415);
-    ctx.fillStyle = color_;
-    ctx.fill();
-    */
-  }
   ~Planet() {}
+
+  void Draw(/* CanvasRenderingContext2D ctx */);
 };
 
 } // namespace nbody
