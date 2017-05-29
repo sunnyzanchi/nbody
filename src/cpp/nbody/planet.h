30be7c04a3aa7a13b8d8a5c2c89d50b77ab6eaad
Nolan Holden <nolanholden@users.noreply.github.com>
2017-05-28 22:18:42 -0400

    Format Planet file

--- src/cpp/nbody/planet.h
+++ src/cpp/nbody/planet.h
@@ -6,24 +6,24 @@
 
 namespace nbody {
 
-  class Planet : public Body, public Drawable {
-  public:
-    Planet(float x, float y, float mass, float radius, std::string color,
-          Vector velocity = Vector(), Vector acceleration = Vector())
-      : Body(x, y, mass, radius, color, velocity, acceleration)
-    {}
+class Planet : public Body, public Drawable {
+public:
+  Planet(float x, float y, float mass, float radius, std::string color,
+        Vector velocity = Vector(), Vector acceleration = Vector())
+    : Body(x, y, mass, radius, color, velocity, acceleration)
+  {}
 
-    // TODO: Implement Draw().
-    void Draw(/* CanvasRenderingContext2D ctx */) {
-      /*
-      ctx.beginPath();
-      ctx.ellipse(x_, y_, radius_, radius_, 0, 0, 2 * 3.1415);
-      ctx.fillStyle = color_;
-      ctx.fill();
-      */
-    }
-    ~Planet() {}
-  };
+  // TODO: Implement Draw().
+  void Draw(/* CanvasRenderingContext2D ctx */) {
+    /*
+    ctx.beginPath();
+    ctx.ellipse(x_, y_, radius_, radius_, 0, 0, 2 * 3.1415);
+    ctx.fillStyle = color_;
+    ctx.fill();
+    */
+  }
+  ~Planet() {}
+};
 
 } // namespace nbody
 
