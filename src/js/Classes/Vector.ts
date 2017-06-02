4669995592d2946c314dd26155526db256ebdb60
Eric Zanchi <emzanchi@gmail.com>
2017-06-02 15:37:55 -0500

    Updated Vector to allow creation with a tuple

--- src/js/Classes/Vector.ts
+++ src/js/Classes/Vector.ts
@@ -4,9 +4,15 @@ export default class Vector implements Point{
   x: number
   y: number
 
-  constructor(x = 0, y = 0){
-    this.x = x;
-    this.y = y;
+  constructor(x: number|[number, number] = 0, y = 0){
+    if(Array.isArray(x)){
+      this.x = x[0];
+      this.y = x[1]
+    }
+    else{
+      this.x = x;
+      this.y = y;
+    }
   }
 
   abs(): Vector{
